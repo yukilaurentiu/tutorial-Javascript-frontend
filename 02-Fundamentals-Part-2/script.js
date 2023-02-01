faef19f5b9cbd74d2e9832ad2269e126a63e0476
Yukiko Laurentiu <yuki.laurentiu@gmail.com>
2023-02-01 08:55:48 -0800

    objects

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -71,3 +71,93 @@ scoreDol = calcAverage(85, 54, 41);
 scoreKoa = calcAverage(23, 34, 27);
 
 checkWinner(scoreDol,scoreKoa);
+// ------------------------------------------------
+
+const friends = ['Michael', 'Steven', 'Peter'];
+friends.push('Jay');
+friends.unshift('John');
+friends.pop(); // last element
+friends.shift(); //first element
+friends.includes('Steven'); //boolean
+
+// ------------------------------------------------
+// challenge 2
+
+const bills = [125, 555, 44];
+
+function calcTip(bill){
+  if(bill >= 50 && bill <= 300){
+     return bill * 0.15;
+  } else {
+    return bill * 0.2; 
+  }
+}
+
+
+// const ti = bills.map((v) => calcTip(v))
+// const to = bills.map((v) => calcTip(v) + v)
+// console.log(to)
+
+let tip = calcTip(bills[0])
+const tips = [tip]
+tip = calcTip(bills[1])
+tips.push(tip + bills[1])
+tip = calcTip(bills[2])
+tips.push(tip + bills[2])
+console.log(tips)
+
+
+const total = [(tips[0] + bills[0])]
+total.push(tips[1] + bills[1])
+total.push(tips[2] + bills[2])
+console.log(total)
+
+// const tips = [calcTip(bills[0]), calcTip(bills[1]),calcTip(bills[2])]
+// ------------------------------------------------
+
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  friends: ['Michael', 'Peter', 'Steven'],
+  birthYear: 1991,
+  job: 'teacher',
+  hasDriverlicense: true,
+
+  calsAge: function(){
+    console.log(this);
+    return 2037 - this.birthYear;
+  },
+
+  calsAge1: function(){
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+
+  getSummary: function(){
+    const answer = this.hasDriverlicense ? 'a' : 'no';
+    return `${this.firstName} is a ${this.calsAge()}-year old ${this.job},
+    and he has ${answer} driver's license.`
+  }
+
+}
+
+console.log(jonas.firstName)
+console.log(jonas['firstName'])
+
+jonas.location = 'Portugal'
+
+//Challenge
+const bestFriend = jonas.friends[0];
+console.log(bestFriend)
+
+console.log(jonas.calsAge());
+console.log(jonas.age);
+
+//Challenge
+console.log(jonas.getSummary());
+// getSummary: function(){
+//   return `${this.firstName} is a ${this.calsAge()}-year old ${this.job},
+//   and he has ${this.hasDriverlicense ? 'a' : 'no'}  driver's license`
+// }
+// ------------------------------------------------
+// Challenge 3
