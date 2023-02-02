bbb67682c446aafc20482a5c06e79dc6718dfbee
Yukiko Laurentiu <yuki.laurentiu@gmail.com>
2023-02-02 12:26:14 -0800

    handling event

--- 03-Developer-Skills/script.js
+++ 03-Developer-Skills/script.js
@@ -1,3 +1,18 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+function printForecast(arr){
+  let str = ''
+  for(let i = 0; i < arr.length; i++){
+    str = str + (`${arr[i]}℃ in ${i + 1} days ... `) 
+  }
+  console.log('... ' + str)
+}
+
+const data1 = [17, 21, 23]
+const data2 = [12, 5, -5, 0, 4]
+
+
+
+printForecast(data1)
+printForecast(data2)
--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -1,1 +1,19 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number!';
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 10;
+
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'No number!';
+  }
+});
