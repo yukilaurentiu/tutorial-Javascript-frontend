3aa1e8be8a1bf9080c3764b1755956a670db6c82
Yukiko Laurentiu <yuki.laurentiu@gmail.com>
2023-02-03 11:23:19 -0800

    changed style when user win

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -9,11 +9,33 @@
 
 // console.log(document.querySelector('.guess').value);
 
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess);
 
-  if (!guess) {
-    document.querySelector('.message').textContent = 'No number!';
+  if (score > 1) {
+    if (!guess) {
+      document.querySelector('.message').textContent = 'No Number!';
+    } else if (guess === secretNumber) {
+      document.querySelector('.message').textContent = 'Correct Number!';
+      document.querySelector('body').style.backgroundColor = '#60b347';
+      document.querySelector('.number').style.width = '30rem'
+    } else if (guess > secretNumber) {
+      document.querySelector('.message').textContent = 'Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else if (guess < secretNumber) {
+      document.querySelector('.message').textContent = 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    }
+  } else {
+    document.querySelector('.message').textContent = 'You lost the game!';
+    document.querySelector('.score').textContent = 0;
   }
 });
