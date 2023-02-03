798f5674db15c0aa863014a1ba14e073b0b9e12c
Yukiko Laurentiu <yuki.laurentiu@gmail.com>
2023-02-03 15:55:46 -0800

    added highscore function

--- 05-Guess-My-Number/script.js
+++ 05-Guess-My-Number/script.js
@@ -9,10 +9,9 @@
 
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-
-document.querySelector('.number').textContent = secretNumber;
+let highscore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -23,8 +22,15 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.message').textContent = 'No Number!';
     } else if (guess === secretNumber) {
       document.querySelector('.message').textContent = 'Correct Number!';
+      document.querySelector('.number').textContent = secretNumber;
       document.querySelector('body').style.backgroundColor = '#60b347';
-      document.querySelector('.number').style.width = '30rem'
+      document.querySelector('.number').style.width = '30rem';
+
+      if(score > highscore){
+        highscore = score;
+        document.querySelector('.highscore').textContent = highscore;
+      }
+
     } else if (guess > secretNumber) {
       document.querySelector('.message').textContent = 'Too high!';
       score--;
@@ -38,4 +44,17 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'You lost the game!';
     document.querySelector('.score').textContent = 0;
   }
+});
+
+// -----------------------------------------------------
+
+document.querySelector('.again').addEventListener('click', function () {
+  console.log('hello')
+  score = 20;
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').textContent = '';
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.number').style.width = '15rem';
 });
