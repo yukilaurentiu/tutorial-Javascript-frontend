ede89b38a2d0a1435ac6bfe1a473bde42db67469
yukiko laurentiu <yuki.laurentiu@gmail.com>
2023-02-28 10:42:01 -0800

    forEach

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -71,8 +71,6 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
-const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
-
 /////////////////////////////////////////////////
 
 // SLICE
@@ -97,3 +95,35 @@ console.log(letters);
 
 // JOIN
 console.log(letters.join(' - '));
+
+// AT
+arr = [23, 11, 64];
+console.log(arr.at(0));
+console.log(arr.slice(-1)[0]); //last element
+console.log(arr.at(-1));
+
+// FOREACH
+const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// for (const movement of movements){
+for (const [i ,movement] of movements.entries()){  
+  if(movement > 0){
+    console.log(`Movement ${i+1} You deposited ${movement}`)
+  } else {
+    // abs is remove - sign
+    console.log(`Movement ${i+1} You withdrew ${Math.abs(movement)}`);
+  }
+}
+
+console.log('----FOREACH-------')
+movements.forEach(function(movement, index, array){
+  if(movement > 0){
+    console.log(`Movement ${index +1} You deposited ${movement}`)
+  } else {
+    // abs is remove - sign
+    console.log(`Movement ${index +1} You withdrew ${Math.abs(movement)}`);
+  }
+})
+
+// 0: function(200)
+//1: function(450) .... 
