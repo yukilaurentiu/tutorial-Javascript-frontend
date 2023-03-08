0ff4c14744181559bcbbc1f5cff1f82f9ada334f
yukiko laurentiu <yuki.laurentiu@gmail.com>
2023-03-08 13:32:21 -0800

    find

--- 11-Arrays-Bankist/challenge.js
+++ 11-Arrays-Bankist/challenge.js
@@ -94,12 +94,29 @@ const calcAverageHumanAge = function(arr){
       return 16 + (age * 4);
     }
   })
+  console.log(newArr)
   const adultDog = newArr.filter(age => age >= 18)
   const average = adultDog.reduce((acc, age) => {
-    return (acc + age) / adultDog.length;
+    // no () parenthesis because acc is the total
+    return acc + age / adultDog.length;
   })
   console.log(average)
 }
 
 
 calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3])
+///////////////////////////////////////
+// Coding Challenge #3
+/* 
+Rewrite the 'calcAverageHumanAge' function from the previous challenge, but this time as an arrow function, and using chaining!
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+*/
+
+const calcAveHumanAge = arr => arr
+  .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+  .filter(age => age >= 18)
+  .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+  
+let answer = calcAveHumanAge([16, 6, 10, 5, 6, 1, 4])
+console.log('chalennge 03', answer)
--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -242,3 +242,11 @@ const totalDepositsUSD = movements
   // .map(mov => mov * eurToUsd)
   .reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositsUSD)
+/////////////////////////////////////////////////
+// find is not return array
+const firstWithdrawal = movements.find(mov => mov < 0)
+console.log(movements, firstWithdrawal);
+
+console.log(accounts);
+const account = accounts.find(acc => acc.owner === 'Jessica Davis');
+console.log(account);
