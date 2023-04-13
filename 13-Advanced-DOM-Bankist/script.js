2d2cc93c2695434f017d2790fc50c3e3eb03a712
yukiko laurentiu <yuki.laurentiu@gmail.com>
2023-04-13 16:35:50 -0600

    propagation

--- 13-Advanced-DOM-Bankist/script.js
+++ 13-Advanced-DOM-Bankist/script.js
@@ -131,3 +131,24 @@ setTimeout(() => h1.removeEventListener('mouseenter', alertH1), 3000)
 ;
 // h1.onmouseenter = function(e){
 //   alert('addEventLister: Great! you are reading')}
+
+// Bubling rgb(255,255,255)
+const randomInt = (min,max)=> Math.floor(Math.random() * (max - min + 1 + min));
+const randomColor = ()=> `rgb(${randomInt(0,255)},${randomInt(0,255)},${randomInt(0,255)} )`
+
+document.querySelector('.nav__link').addEventListener('click', function(e) {
+  this.style.backgroundColor = randomColor();
+  // e.currentTareget === this
+  console.log('LINK',e.target, e.currentTarget )
+
+  // Stop ptopagation
+  // e.stopPropagation();
+});
+document.querySelector('.nav__links').addEventListener('click', function(e) {
+  this.style.backgroundColor = randomColor();
+  console.log('CONTAINER',e.target, e.currentTarget)
+});
+document.querySelector('.nav').addEventListener('click', function(e) {
+  this.style.backgroundColor = randomColor();
+  console.log('NAV',e.target, e.currentTarget)
+});
